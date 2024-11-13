3f1880a7127ac96cd429c0fe09b191856b77817e
pavel-sobchenko <pavel.sobchanka@gmail.com>
2024-11-13 17:26:18 +0100

    fix: clear data

--- src/app/shared/constants/errors.ts
+++ src/app/shared/constants/errors.ts
@@ -6,7 +6,5 @@ export const ERRORS = new InjectionToken<Record<string, string>>('ERRORS', {
 
 export const errors: Record<string, string> = {
   required: 'Field is required',
-  email: 'Field should be in email format',
-  requiredFileType: 'File extension is not allowed',
-  dimensionError: 'File dimension is not allowed'
+  email: 'Field should be in email format'
 }
